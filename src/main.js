6a11f1c896bcefe73aa7e59892df38fa141a44af
Miguel Minguez <m.minguezz@gmail.com>
2021-07-16 22:48:56 +0200

    feat(component): update menu / header

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
+(function() {
+  var menuBurguer = document.querySelector(".js-menu-toggle");
+  var menu = document.querySelector(".js-menu");
 
-var menuBurguer = document.querySelector(".js-menu-toggle");
-var menu = document.querySelector(".js-menu");
+  menuBurguer.addEventListener("click", function(event) {
+    var menuOpen = menu.classList.contains("is-open");
+    var menuStatus = !menuOpen;
 
-menuBurguer.addEventListener("click", function(event) {
-  var menuOpen = menu.classList.contains("is-open");
-  var menuStatus = !menuOpen;
-  
-  menuBurguer.setAttribute("aria-expanded", menuStatus);
-  menu.classList.toggle("is-open");
-});
+    menuBurguer.setAttribute("aria-expanded", menuStatus);
+    menu.classList.toggle("is-open");
+  });
+})();
