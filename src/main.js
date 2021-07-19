567208da925a4d67adec0c2e8be65e258dcbd8af
Miguel Minguez <m.minguezz@gmail.com>
2021-07-19 18:36:06 +0200

    feat(component): init tabs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 (function() {
+  // menu
   var menuBurguer = document.querySelector(".js-menu-toggle");
   var menu = document.querySelector(".js-menu");
 
@@ -9,4 +10,56 @@
     menuBurguer.setAttribute("aria-expanded", menuStatus);
     menu.classList.toggle("is-open");
   });
+  
+  
+  // tabs - https://inclusive-components.design/
+  const tabs = document.querySelector('.js-tabs');
+  const tablist = tabs.querySelector('ul');
+  const tabsitems = tablist.querySelectorAll('li');
+  const tabslinks = tablist.querySelectorAll('a');
+  const panels = tabs.querySelectorAll('[id^="section"]');
+  
+  const switchTab = (oldTab, newTab) => {
+    newTab.focus();
+    newTab.removeAttribute('tabindex');
+    newTab.setAttribute('aria-selected', 'true');
+    oldTab.removeAttribute('aria-selected');
+    oldTab.setAttribute('tabindex', '-1');
+    
+    let index = Array.prototype.indexOf.call(tabslinks, newTab);
+    let oldIndex = Array.prototype.indexOf.call(tabslinks, oldTab);
+    
+    tabsitems[oldIndex].classList.remove("is-active");
+    tabsitems[index].classList.add("is-active");
+    panels[oldIndex].hidden = true;
+    panels[index].hidden = false;
+  };
+  
+  Array.prototype.forEach.call(tabslinks, (tab, i) => {
+    tab.addEventListener('click', e => {
+      e.preventDefault();
+      let currentTab = tablist.querySelector('[aria-selected]');
+      if (e.currentTarget !== currentTab) {
+        switchTab(currentTab, e.currentTarget);
+      }
+    });
+    
+    // Handle keydown events for keyboard users
+    tab.addEventListener('keydown', e => {
+      // Get the index of the current tab in the tabs node list
+      let index = Array.prototype.indexOf.call(tabslinks, e.currentTarget);
+      // Work out which key the user is pressing and
+      // Calculate the new tab's index where appropriate
+      let dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
+      if (dir !== null) {
+        e.preventDefault();
+        // If the down key is pressed, move focus to the open panel,
+        // otherwise switch to the adjacent tab
+        dir === 'down' ? panels[i].focus() : tabslinks[dir] ? switchTab(e.currentTarget, tabslinks[dir]) : void 0;
+      }
+    });
+  });
+  
+  
+  
 })();
